bf3c9622f20d58b048886658508f4c7296c880e4
Felipe Pichl <felipe.pichl@hotmail.com>
2022-03-18 22:28:35 -0300

    Test - PUT/todos/:id

--- src/index.js
+++ src/index.js
@@ -70,7 +70,16 @@ app.post('/todos', checksExistsUserAccount, (request, response) => {
 });
 
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { id } = request.params;
+  const { title, deadline } = request.body;
+  const { user } = request;
+
+  const todo = user.todos.find(operation => operation.id === id);
+
+  todo.title = title,
+  todo.deadline = new Date(deadline);
+
+  return response.status(200).json(todo);
 });
 
 app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
